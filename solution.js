5ff5fff5701bc46b66a85dfb145330c7bf77599e
Parker Couch <parkercouch@gmail.com>
2019-03-09 18:47:11 -0300

    Added decoder solution

--- solution.js
+++ solution.js
@@ -30,36 +30,32 @@ let decoderTable = {
 };
 
 // These are the inputs that you have
-let inputs = [[2, 6], [0, 5], [9, 3], [4, 8], [10, 5], 'BOP', [11, 12], [5, 10], [1, 17], [5, 7], [4, 0]];
+const input1 = [[2, 6], [0, 5], [9, 3], [4, 8], [10, 5], 'BOP', [11, 12], [5, 10], [1, 17], [5, 7], [4, 0]];
+const input2 = [[1, 8], 'BOP', [5, 7], [13, 2], [9, 13], [1, 4], 'BOP', [2, 1], [0, 1], [10, 10], [10, 9]];
+const input3 = [[9, 16], [13, 2], [13, 8], 'BOP', [2, 1], [0, 1], [10, 4], 'BOP', [2, 2], [4, 11], 'BOP', [4, 9], [8, 7], [7, 11], [3, 2], 'BOP', [11, 9], [3, 5], [0, 1], [7, 7], 'BOP', [13, 12], [13, 2], [8, 13], 'BOP', [5, 6], [12, 2], [9, 6], [20, 3]];
+const input4 = [[1, 0], 'BOP', [2, 1], [3, 5], [0, 1], [4, 9], [6, 10], [3, 6], [4, 11], [8, 6], 'BOP', [2, 7], [12, 7], 'BOP', [3, 1], [5, 0], [1, 5], [4, 5], [12, 2], [2, 3], [1, 3], 'BOP', [9, 5], [12, 3], [14, 6], 'BOP', [1, 1], [22, 3], 'BOP', [8, 12], [4, 4], [4, 1], [8, 1], [11, 7], 'BOP', [12, 11], [3, 6], [8, 6], [12, 7], 'BOP', [0, 2], [10, 11], [11, 9], 'BOP', [2, 0], [25, 0], 'BOP', [3, 5], [10, 5], [10, 13], 'BOP', [11, 9], [3, 5], [4, 1], [17, 8], 'BOP', [2, 1], [1, 0], [5, 9], 'BOP', [16, 2], [1, 4], [3, 0], [7, 8], [9, 13], [2, 3], [9, 9], 'BOP', [12, 11], [4, 4], [5, 0], [8, 6], 'BOP', [11, 9], [3, 5], [4, 1], [17, 8], 'BOP', [0, 6], [1, 0], [9, 3], [0, 12]];
 
+const inputs = [input1, input2, input3, input4];
 
-// STEP 1: Make a new, empty array called letters.
+const combineBeepsBoops = function (input) {
+  if (Array.isArray(input)) {
+    return input.reduce((a, b) => a + b);
+  }
+  if (input === 'BOP') {
+    return 'BOP';
+  }
+  return 0;
+};
 
-// STEP 2: Add the total number of beeps + boops to the letters array. This will
-// include looping over the inputs array to get the totals.
+const decode = function (key) {
+  return (function extractKey(i) { return (i in key) ? key[i] : 0; });
+};
 
-// In other words, for each array inside the inputs array (starting with [2, 6]),
-// the left number (2) is the number of beeps and the right number (6) is the
-// number of boops. Add those together to get 8. Add the number 8 to the letters array.
+const decodeMessage = function (input, key) {
+  return input
+        .map(combineBeepsBoops)
+        .map(decode(key))
+        .join('');
+};
 
-// HINT: Remember to deal with the 'BOP's! You can either ignore them or just pass them directly
-// into the letters array
-
-
-// STEP 3: Stop and check. Let's print out our letters array and make sure it contains the
-// expected values! Uncomment the below code to check your answers.
-
-// console.log('expected values:');
-// console.log([8, 5, 12, 12, 15, 'BOP', 23, 15, 18, 12, 4])
-// console.log('actual values:');
-// console.log(letters);
-
-// STEP 4: Great - we have all the totals and we have the decoder table.
-// Together, these can tell us what the secret message is!
-
-// HINT: We need to loop over the letters array. For each item, we need to use it
-// as a key in the decoder table. Print the result!
-
-
-
-
+inputs.forEach(i => console.log(decodeMessage(i, decoderTable)));
